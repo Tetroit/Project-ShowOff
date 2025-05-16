067e25c915221e0807cdad2d6834483b19b02394
Tetroit <shichinov.ivan@gmail.com>
2025-05-16 20:29:28 +0200

    Climbing spots added

---
+++ Assets/Scripts/Player/Triggers/ClimbCutsceneTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace amogus
+{
+    public class ClimbCutsceneTrigger : CutsceneTrigger<PlayerFSM>
+    {
+        [field: SerializeField] public Vector3 targetPosLocal { get; private set; }
+        [SerializeField] private ClimbCutsceneAnimation cutscene;
+        public override ScriptedAnimation<PlayerFSM> Cutscene => cutscene;
+        public override Predicate<PlayerFSM> Predicate => (PlayerFSM player) => {
+            return InputSystem.actions.FindActionMap("Player").FindAction("Interact").WasPressedThisFrame();
+        };
+
+        [ExecuteInEditMode]
+        private void OnDrawGizmos()
+        {
+            Gizmos.matrix = Matrix4x4.Translate(transform.position);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(targetPosLocal, 0.1f);
+        }
+    }
+    [Serializable]
+    public class ClimbCutsceneAnimation : ScriptedAnimation<PlayerFSM>
+    {
+        Vector3 startPos;
+        [SerializeField] ClimbCutsceneTrigger trigger;
+        Vector3 targetPos => trigger.targetPosLocal + trigger.transform.position;
+        public override void Animate(PlayerFSM target)
+        {
+            target.transform.position = Vector3.Lerp(startPos, targetPos, time01);
+        }
+        public override void Begin(PlayerFSM target)
+        {
+            startPos = target.transform.position;
+            target.DisableControls();
+        }
+        public override void End(PlayerFSM target)
+        {
+            target.EnableControls();
+        }
+
+    }
+}
