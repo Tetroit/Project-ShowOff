1358e3c063ee9f729f4de0505f3e1c5148d5f8c7
RianneTV <138168573+RianneTV@users.noreply.github.com>
2025-05-20 09:23:23 +0200

    Menus and pausescreen
    Still need:
    - HUD
    - Audio sliders on Settings page & paused screen

---
+++ Assets/Scripts/UI/ButtonsHUD.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class ButtonsHUD : MonoBehaviour
+{
+    // Canvases
+    [SerializeField] private GameObject _menuCanvas;
+    [SerializeField] private GameObject _gameCanvas;
+
+    // Pages
+    [SerializeField] private GameObject _unpausedView;
+    [SerializeField] private GameObject _pausedView;
+    [SerializeField] private GameObject _pMainPage;
+    [SerializeField] private GameObject _pControlsPage;
+    [SerializeField] private GameObject _pAudioPage;
+
+    // Variables
+    private bool _isPaused = false;
+
+    public void QuitMenu()
+    { // Return to menu stuff here
+        _isPaused = false;
+        _menuCanvas.SetActive(true);
+        _pausedView.SetActive(false);
+        _gameCanvas.SetActive(false);
+    }
+    public void ClickControls()
+    {
+        _pMainPage.SetActive(false);
+        _pControlsPage.SetActive(true);
+    }
+    public void ClickAudio()
+    {
+        _pMainPage.SetActive(false);
+        _pAudioPage.SetActive(true);
+    }
+    public void ClickMain()
+    {
+        _pControlsPage.SetActive(false);
+        _pAudioPage.SetActive(false);
+        _pMainPage.SetActive(true);
+    }
+    public void UnpauseGame()
+    {
+        _isPaused = false;
+        _pControlsPage.SetActive(false);
+        _pAudioPage.SetActive(false);
+        _pMainPage.SetActive(true);
+        _pausedView.SetActive(false);
+        _unpausedView.SetActive(true);
+    }
+    public void PauseGame()
+    {
+        _isPaused = true;
+        _pausedView.SetActive(true);
+        _unpausedView.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                UnpauseGame();
+            }
+            else if (!_isPaused)
+            {
+                PauseGame();
+            }
+        }
+    }
+}
