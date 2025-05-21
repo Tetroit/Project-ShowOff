9e55b37d988388e06e0b972b891df8c6cff39536
Tetroit <shichinov.ivan@gmail.com>
2025-05-21 13:01:28 +0200

    Falling animation

--- Assets/Scripts/Player/Triggers/FloorFallCutsceneTrigger.cs
+++ Assets/Scripts/Player/Triggers/FloorFallCutsceneTrigger.cs
@@ -3,6 +3,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 namespace amogus
 {
@@ -10,6 +11,11 @@ namespace amogus
     {
         [SerializeField] private FloorFallCutsceneAnimation cutscene;
 
+        public Matrix4x4 offset => Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        public Vector3 targetPos;
+        public Quaternion targetFacing;
+        public Vector3 targetPosGS => transform.TransformPoint(targetPos);
+        public Quaternion targetFacingGS => targetFacing;
         public override ScriptedAnimation<PlayerFSM> Cutscene => cutscene;
         public override Predicate<PlayerFSM> Predicate => (PlayerFSM player) => {
             return true;
@@ -19,23 +25,57 @@ namespace amogus
         {
             base.Trigger();
         }
+        [ExecuteAlways]
+        private void OnDrawGizmos()
+        {
+            cutscene.DrawGizmos();
+        }
     }
     [Serializable]
     public class FloorFallCutsceneAnimation : ScriptedAnimation<PlayerFSM>
     {
+        [SerializeField] FloorFallCutsceneTrigger trigger;
         [SerializeField] PlayableDirector director;
+
+        Vector3 startPos;
+        Quaternion startRot;
+
+        bool targetAligned = false;
         public override void Animate(PlayerFSM target)
         {
-            director = target.GetComponent<PlayableDirector>();
-            director.Play();
+            if (time < 1)
+            {
+                target.transform.position = Vector3.Lerp(startPos, trigger.targetPosGS, time);
+                target.transform.rotation = Quaternion.Slerp(startRot, trigger.targetFacingGS, time);
+            }
+            else
+            {
+                if (!targetAligned)
+                {
+                    targetAligned = true;
+                    director.Play();
+                }
+            }
         }
         public override void Begin(PlayerFSM target)
         {
-
+            director = target.GetComponent<PlayableDirector>();
+            startPos = target.transform.position;
+            startRot = target.transform.rotation;
+            target.DisableControls();
         }
         public override void End(PlayerFSM target)
         {
             director.Stop();
+            target.EnableControls();
+        }
+
+        public void DrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(trigger.targetPosGS, 0.2f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(trigger.targetPosGS, trigger.targetPosGS + trigger.targetFacingGS * Vector3.forward);
         }
     }
 }
