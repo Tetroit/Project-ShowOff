b709638c01967d93b0ae6f03cc2913953c7eeec4
Birka <birkavidit@gmail.com>
2025-06-05 13:30:56 +0200

    added connor sounds into main scene, added ghost step camera shake

--- Assets/Scripts/Game Elements/FollowingSteps.cs
+++ Assets/Scripts/Game Elements/FollowingSteps.cs
@@ -1,43 +1,51 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Unity.VisualScripting;
+﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace amogus
 {
     public class FollowingSteps : MonoBehaviour
     {
-        [SerializeField] float speed;
-
+        [SerializeField] float _speed;
         [SerializeField] Curve curve;
+        [SerializeField] AnimationCurve _timeCurve01;
+        [SerializeField] PlayAudioOnDistanceTraveled _footSteps;
+        [SerializeField] AnimationCurve _footStepDistanceCHanger;
+        [Header("Camera Shake")]
+        [SerializeField] CameraWalkingShake _cameraShake;
+        [SerializeField] float _time;
+        [SerializeField] float _scale;
+        [SerializeField] float _frequency;
 
         Coroutine movement;
         public void StartFollowing()
         {
             movement = StartCoroutine(nameof(Step));
+            _footSteps.OnStep.AddListener(OnStepSound);
         }
         public void EndFollowing()
         {
-            if (movement != null)
-                StopCoroutine(movement);
+            if (movement != null) StopCoroutine(movement);
+            _footSteps.OnStep.RemoveListener(OnStepSound);
+
         }
 
         IEnumerator Step()
         {
             float dist = curve.Getlength();
-            float fac = 0;
-            while (fac < 1)
+            float time01 = 0;
+            while (time01 < 1)
             {
-                fac += speed / dist * Time.deltaTime;
-                transform.position = curve.GetPositionFromDistanceGS(fac * dist);
+                time01 += _speed / dist * Time.deltaTime;
+                transform.position = curve.GetPositionFromDistanceGS(_timeCurve01.Evaluate(time01) * dist);
+                _footSteps.DistanceUntilStep = _footStepDistanceCHanger.Evaluate(time01);
                 yield return new WaitForEndOfFrame();
             }
             yield break;
+        }
+
+        void OnStepSound()
+        {
+            _cameraShake.Shake(_time, _scale, _frequency);
         }
 
         public void Start()
--- Assets/Scripts/Player/CameraShake.cs
+++ Assets/Scripts/Player/CameraShake.cs
@@ -1,8 +1,7 @@
-
 using amogus;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class CameraWalkingShake : MonoBehaviour
 {
@@ -13,46 +12,30 @@ public class CameraWalkingShake : MonoBehaviour
         SPRINTING = 2,
         LADDER = 3,
     }
-
-    Vector3 offset;
-    float rotationOffset;
+    
     public float frequency => currentShakeState.frequency;
     public float rotationIntensity => currentShakeState.rotationIntensity;
-    float _cooldown;
-
-    public bool soundRotation = true;
     public AnimationCurve XShake => currentShakeState.XShake;
     public AnimationCurve YShake => currentShakeState.YShake;
     public bool resetOnDoingNothing => currentShakeState.resetOnDoingNothing;
+    public CameraShakeState currentShakeState => states[(int)currentShakeStateID];
+
+    public bool soundRotation = true;
     public PlayerFSM controls;
 
     [SerializeField] List<CameraShakeState> states;
-    State currentShakeStateID = 0;
-    public CameraShakeState currentShakeState => states[(int)currentShakeStateID];
-    //public AudioSource stepSource;
-    //public AudioClip[] stepSounds;
+    [SerializeField] private FootstepsPlayer _footstepsPlayer;
 
+    State currentShakeStateID = 0;
     bool _rightSide;
     float _amplitude = 0;
-    //int _currentClipID = 0;
+    Vector3 offset;
+    float rotationOffset;
+    float _cooldown;
 
-    [SerializeField] private FootstepsPlayer _footstepsPlayer;
-    
 
-    void Start()
-    {
-        //if (stepSource == null)
-        //{
-        //    stepSource = GetComponent<AudioSource>();
-        //    if (stepSource == null)
-        //    {
-        //        stepSource = gameObject.AddComponent<AudioSource>();
-        //        stepSource.outputAudioMixerGroup = Settings.main.GetMixerChannel(Settings.PLAYER_GROUP);
-        //    }
-        //}
-    }
+    Stack<Vector3> _oneShotOffsets = new();
 
-    // Update is called once per frame
     void Update()
     {
         if (controls.isMoving) _amplitude = Mathf.Lerp(_amplitude, 1, 0.01f);
@@ -65,30 +48,13 @@ public class CameraWalkingShake : MonoBehaviour
                 _rightSide = !_rightSide;
                 if(FMODEvents.instance != null)
                 {
-                    //AudioManager.instance.PlayOneShot(FMODEvents.instance.footSteps, transform.position);
                     _footstepsPlayer.PlayFootstepSound();
                 }
-
-                //if (stepSource != null && stepSounds.Length != 0)
-                //{
-                //    if (soundRotation)
-                //    {
-                //        _currentClipID++;
-                //        if (_currentClipID == stepSounds.Length) _currentClipID = 0;
-                //        stepSource.clip = stepSounds[_currentClipID];
-                //    }
-                //    else
-                //    {
-                //        stepSource.clip = stepSounds[Random.Range(0, stepSounds.Length)];
-                //    }
-                //    stepSource.Play();
-                //}
             }
         }
         else
         {
-            //if (resetOnDoingNothing || controls.isMoving)
-                _cooldown += Time.deltaTime;
+            _cooldown += Time.deltaTime;
         }
         float facY = _cooldown / frequency;
         float facX = facY;
@@ -98,9 +64,33 @@ public class CameraWalkingShake : MonoBehaviour
         rotationOffset = -rotationIntensity * offset.x;
 
         transform.localEulerAngles = new Vector3(0f, 0f, rotationOffset);
-        transform.localPosition = offset;
+        Vector3 oneShotOffsets = new();
+        while(_oneShotOffsets.Count > 0)
+        {
+            oneShotOffsets += _oneShotOffsets.Pop();
+        }
 
+
+        transform.localPosition = offset + oneShotOffsets;
     }
+
+    public void Shake(float time, float scale, float frequency)
+    {
+        StartCoroutine(OneShotShake(time, scale, frequency));
+    }
+
+    IEnumerator OneShotShake(float time, float scale, float frequency)
+    {
+        var wait = new WaitForSeconds(frequency);
+        while(time > 0)
+        {
+            var oneShotOffset = new Vector3(UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f)) * scale;
+            _oneShotOffsets.Push(oneShotOffset);
+            yield return wait;
+            time -= Time.deltaTime;
+        }
+    }
+
 
     public void ChangeState(State ID)
     {
