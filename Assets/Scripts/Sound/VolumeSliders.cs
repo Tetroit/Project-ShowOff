e9e1db6a7bd64a63fec11aded87f0db837b568fd
Connor Smith <545278@student.saxion.nl>
2025-06-11 14:56:32 +0200

    Volume sliders implemented and functional

--- Assets/Scripts/Sound/VolumeSliders.cs
+++ Assets/Scripts/Sound/VolumeSliders.cs
@@ -1,11 +1,21 @@
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class VolumeSliders : MonoBehaviour
 {
-    [SerializeField] private string Parameter;
+    [SerializeField] private string Usedbus;
+    private Bus bus;
+
+    private void Start()
+    {
+        bus = RuntimeManager.GetBus(Usedbus); 
+
+    }
 
     public void ChangeVolume(float volume)
     {
-       
+         bus.setVolume(volume);
+        Debug.Log(volume);
     }
 }
