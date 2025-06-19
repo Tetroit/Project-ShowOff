6412aa53471a286b5763e7169c16a817b2f70768
Birka <birkavidit@gmail.com>
2025-06-19 11:48:03 +0200

    added hand animations

--- Assets/Scripts/Stefan/Inventory/InventoryItemView.cs
+++ Assets/Scripts/Stefan/Inventory/InventoryItemView.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class InventoryItemView : MonoBehaviour
 {
-
+    public UnityEvent OnSelect;
+    public UnityEvent OnDeselect;
     public virtual void Select()
     {
         if (gameObject != null)
             gameObject.SetActive(true);
-
+        OnSelect?.Invoke();
     }
 
     public virtual void Deselect()
     {
         if(gameObject != null)
             gameObject.SetActive(false);
+        OnDeselect?.Invoke();
 
     }
 
