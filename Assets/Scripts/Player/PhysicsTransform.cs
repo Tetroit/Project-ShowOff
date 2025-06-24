e6167ee21cab1146334823e17c0ac3bd3f5f9902
Birka <birkavidit@gmail.com>
2025-06-24 15:36:30 +0200

    clunky arm colider is there

--- Assets/Scripts/Player/PhysicsTransform.cs
+++ Assets/Scripts/Player/PhysicsTransform.cs
@@ -3,19 +3,13 @@ using UnityEngine;
 public class PhysicsTransform : MonoBehaviour
 {
     [SerializeField] Transform _parent;
-    [SerializeField] Vector3 _positionOffset;
-    [SerializeField] Quaternion _rotationOffset;
     Rigidbody _body;
-    Vector3 _worldPositionOffset;
-    Quaternion _worldRotationOffset;
 
 
 
     void Start()
     {
         _body = GetComponent<Rigidbody>();
-        _worldPositionOffset = transform.TransformPoint(_positionOffset);
-        _worldRotationOffset = transform.parent.rotation * _rotationOffset;
         transform.position = _parent.position;
     }
 
@@ -24,7 +18,7 @@ public class PhysicsTransform : MonoBehaviour
         if (_body == null) return;
 
 
-        _body.MoveRotation(_parent.rotation );
-        _body.MovePosition(_parent.position +_positionOffset);
+        _body.MoveRotation(_parent.rotation);
+        _body.MovePosition(_parent.position);
     }
 }
