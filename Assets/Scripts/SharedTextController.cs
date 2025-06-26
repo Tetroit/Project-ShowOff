d5dba5b12d43f6bdf66108e0c0044c52741ab649
Birka <birkavidit@gmail.com>
2025-06-26 17:01:01 +0200

    lighting issues fixed, made baked lighting

---
+++ Assets/Scripts/SharedTextController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SharedTextController : MonoBehaviour
+{
+    [SerializeField] SharedTextData _sharedData;
+    private void OnEnable()
+    {
+        
+    }
+}
--- Assets/Scripts/Utils/LightFlicker.cs
+++ Assets/Scripts/Utils/LightFlicker.cs
@@ -7,7 +7,12 @@ public class LightFlicker : MonoBehaviour
     [SerializeField] Vector2 _ligtOnDurationMinMax = new(5f,10f);
     [SerializeField] Vector2 _lightOutDurationMinMax = new(0.1f,.4f);
     [SerializeField] Light _light;
+    [SerializeField] Material _onMaterial;
+    [SerializeField] Material _offMaterial;
+    [SerializeField] MeshRenderer _bulbRenderer;
+
     Coroutine _coroutine;
+
     void OnEnable()
     {
         if(_light ==  null) _light = GetComponentInChildren<Light>(true);
@@ -25,6 +30,8 @@ public class LightFlicker : MonoBehaviour
     IEnumerator LightOn(float time)
     {
         _light.enabled = true;
+        _bulbRenderer.material = _onMaterial;
+
         yield return new WaitForSeconds(time);
         StartCoroutine(LightOff(Random.Range(_lightOutDurationMinMax.x, _lightOutDurationMinMax.y)));
     }
@@ -32,6 +39,8 @@ public class LightFlicker : MonoBehaviour
     IEnumerator LightOff(float time)
     {
         _light.enabled = false;
+        _bulbRenderer.material = _offMaterial;
+
         yield return new WaitForSeconds(time);
         StartCoroutine(LightOn(Random.Range(_ligtOnDurationMinMax.x, _ligtOnDurationMinMax.y)));
 
